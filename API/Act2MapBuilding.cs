a9f7ca428794401f148617f2d13fef6704e842f0
rspforhp <39800618+rspforhp@users.noreply.github.com>
2023-01-21 23:00:37 +0500

    Basic api stuff, added a way to make entrances without actually needing to switch scenes, still need a way to handle adding stuff inside them tho

---
+++ API/Act2MapBuilding.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using DiskCardGame;
+using GBC;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Act2Tower.API
+{
+    public static class Act2MapBuilding
+    {
+        public static GameObject CreateGBCGameObject(string name, [CanBeNull] GameObject MapBase,
+            [CanBeNull] Vector3 pos, [CanBeNull] Sprite sprite)
+        {
+            var go = new GameObject(name, new[] { typeof(SpriteRenderer) });
+            if (MapBase != null) go.transform.parent = MapBase.transform;
+            go.GetComponent<SpriteRenderer>().sprite = sprite;
+            go.layer = LayerMask.NameToLayer("GBCPixel");
+            go.transform.position = pos;
+            return go;
+        }
+
+        public static GameObject GBCCamerasPrefab => Resources.Load<GameObject>("prefabs\\gbcoverworld\\GBCCameras");
+
+        public static GameObject GBCSingletonsPrefab =>
+            Resources.Load<GameObject>("prefabs\\gbcoverworld\\GBCSingletons");
+
+        public static GameObject FreeMovePlayerPrefab =>
+            Resources.Load<GameObject>("prefabs\\gbcinterior\\FreeMovePlayer");
+
+        public static GameObject TryCreateGBCEntranceScene(string name = "SCENE")
+        {
+            var go = new GameObject(name);
+            GameObject.Instantiate(GBCCamerasPrefab).transform.parent = go.transform;
+            GameObject.Instantiate(FreeMovePlayerPrefab).transform.parent = go.transform;
+            go.SetActive(false);
+            go.hideFlags = HideFlags.HideAndDontSave;
+            return go;
+        }
+
+        public static ZoneEntrance AddNavigationZoneEntrance(GameObject navigationZoneObject,
+            string nameOfSceneToSwitch, LookDirection dir)
+        {
+            var e = navigationZoneObject.AddComponent<ZoneEntrance>();
+            var zone = navigationZoneObject.GetComponent<NavigationZone2D>();
+            if (zone.navigationEvents == null) zone.navigationEvents = new List<NavigationZone2D.NavigationEvent>();
+            zone.navigationEvents.Add(new NavigationZone2D.NavigationEvent()
+                { direction = dir, action = delegate { e.EnterZone(); } });
+            e.zoneSceneId = nameOfSceneToSwitch;
+            return e;
+        }
+
+        private static void SwitchToEntranceScene(GameObject scene)
+        {
+            foreach (var gameObject in GameObject.FindObjectsOfType<GameObject>().ToList().FindAll(g => g.activeInHierarchy))
+            {
+                if(gameObject.name!="CursorClone"&& gameObject.name!="CustomCoroutineRunner"&&gameObject.name!=GBCSingletonsPrefab.name&& gameObject.scene.name==("GBC_WorldMap") && gameObject.hideFlags==HideFlags.None) 
+                    gameObject.SetActive(false);
+            }
+
+            scene.SetActive(true);
+        }
+
+        public static void AddNavigationZoneEntrance(GameObject navigationZoneObject,
+            GameObject EntranceScene, LookDirection dir)
+        {
+            var zone = navigationZoneObject.GetComponent<NavigationZone2D>();
+            if (zone.navigationEvents == null) zone.navigationEvents = new List<NavigationZone2D.NavigationEvent>();
+            zone.navigationEvents.Add(new NavigationZone2D.NavigationEvent()
+                { direction = dir, action = delegate { SwitchToEntranceScene(EntranceScene); } });
+        }
+
+        public static GameObject CreateGBCNaviationZone(string name, GameObject MapBase, Vector3 pos,
+            List<LookDirection> blockedDirections, int x, int y)
+        {
+            var go = new GameObject(name, new[] { typeof(NavigationZone2D) });
+            go.transform.parent = MapBase.transform.parent.GetChild(0);
+            var navZone = go.GetComponent<NavigationZone2D>();
+            navZone.blockedDirections = blockedDirections;
+            go.transform.position = pos;
+            var navigationGrid = NavigationGrid.instance;
+            if (navigationGrid.zones[x, y] != null)
+                Act2Tower._Logger.LogError("Tried to add a nav zone into not an empty space");
+            else if (navigationGrid.zones.GetLength(0) < x || navigationGrid.zones.GetLength(1) < y)
+                Act2Tower._Logger.LogError("Tried to add a nav zone outside of bounds");
+            else
+                navigationGrid.zones[x, y] = navZone;
+            return go;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Act2Tower.API;
+using BepInEx;
+using BepInEx.Logging;
+using GBC;
+using InscryptionAPI.Helpers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Act2Tower
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class Act2Tower : BaseUnityPlugin
+    {
+        public const string PluginGuid = "inscryption.kopie.act2tower";
+        public const string PluginName = "Act2Tower";
+        public const string PluginVersion = "0.0.1";
+        internal static Act2Tower ModInstance;
+        internal static ManualLogSource _Logger => ModInstance.Logger;
+
+        public static event Action<Scene,GameObject> OnGBCMapLoaded;
+
+
+        public void GBCMapLoaded_Handling(Scene s, GameObject MapBase)
+        {
+            Vector3 pos = new Vector3(0.03f, -0.3f, 0);
+            string name = "Test of bridge";
+            Sprite bridgeSpr = TextureHelper.GetImageAsTexture("Artwork\\Bridge.png").ConvertTexture();
+            var testBridge = Act2MapBuilding.CreateGBCGameObject(name, MapBase, pos, bridgeSpr);
+            var testBridgeNavZone=Act2MapBuilding.CreateGBCNaviationZone(name, MapBase, pos, new List<LookDirection>(), 3, 3);
+            var testBridgeInside = Act2MapBuilding.TryCreateGBCEntranceScene("BridgeInsides");
+            Act2MapBuilding.AddNavigationZoneEntrance(testBridgeNavZone, testBridgeInside,LookDirection.South);
+        }
+
+        private void Awake()
+        {
+            ModInstance = this;
+            Logger.LogMessage($"Mod {nameof(Act2Tower)} loaded!");
+            SceneManager.sceneLoaded+= delegate(Scene argScene, LoadSceneMode mode)
+            {
+                if (argScene.name == "GBC_WorldMap")
+                {
+                   if(OnGBCMapLoaded!=null)OnGBCMapLoaded(argScene, GameObject.Find("Map").transform.GetChild(1).gameObject);
+                }
+            };
+            OnGBCMapLoaded += GBCMapLoaded_Handling;
+            
+            
+
+
+
+
+        }
+    }
+}
