811fc7aca2e97bcba39457a127868381ddca22e2
rspforhp <39800618+rspforhp@users.noreply.github.com>
2023-03-31 16:14:03 +0500

    sTUFF

--- Plugin.cs
+++ Plugin.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using Act2Tower.API;
+using System.IO;
+using System.Linq;
 using BepInEx;
 using BepInEx.Logging;
+using DiskCardGame;
 using GBC;
+using HarmonyLib;
 using InscryptionAPI.Helpers;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 namespace Act2Tower
@@ -19,102 +24,57 @@ namespace Act2Tower
         internal static Act2Tower ModInstance;
         internal static ManualLogSource _Logger => ModInstance.Logger;
 
-        public static event Action<Scene,GameObject> OnGBCMapLoaded;
-        public Sprite TowerSprite;
-        public Sprite TowerFloorSprite;
-        public void GBCMapLoaded_Handling(Scene s, GameObject MapBase)
-        {
-            Vector3 pos = new Vector3(0.03f, -0.3f, 0);
-            string name = "Test of bridge";
-            Sprite bridgeSpr = TextureHelper.GetImageAsTexture("Artwork\\Bridge.png").ConvertTexture();
-            var testBridge = Act2MapBuilding.CreateGBCGameObject(name, MapBase, pos, bridgeSpr);
-            Act2MapBuilding.CreateGBCGameObject("InfinitTowerSprite", MapBase, pos+new Vector3(-0.25f,0.12f,0), TowerSprite);
-            var testBridgeNavZone=Act2MapBuilding.CreateGBCNaviationZone(name, MapBase, pos, new List<LookDirection>(), 3, 3);
-            var testBridgeInside = Act2MapBuilding.TryCreateGBCEntranceScene("BridgeInsides",true);
-            Act2MapBuilding.AddNavigationZoneEntrance(testBridgeNavZone, testBridgeInside,LookDirection.West);
-            Act2MapBuilding.OnGBCZoneSceneLoaded+= delegate(GameObject o)
-            {
-                if (o == testBridgeInside)
-                {
-                    Logger.LogMessage($"A custom zone entered;");
-                    GBC.PlayerMovementController.Instance.transform.position = new Vector3(0, -0.9f, 0);
-                    var floor = new GameObject("Floor");
-                    floor.transform.position = new Vector3(0, 0, 0);
-                    floor.transform.localScale = new Vector3(1.1f, 1, 1);
-                    floor.layer = LayerMask.NameToLayer("GBCPixel");
-                    var collidersStor = new GameObject("Colliders");
-                    collidersStor.transform.parent = floor.transform;
-                    collidersStor.transform.localPosition = new Vector3(-0.3325f, -0.0531f, -1.8481f);
-                    for (int i = 0; i < 4; i++)
-                    {
-                        var colliderG = new GameObject("Collider " + i);
-                        var collider = colliderG.AddComponent<BoxCollider2D>();
-                        Vector3 localPos=Vector3.zero;
-                        Vector2 collSize=Vector2.zero;
-                        switch (i)
-                        {
-                            case 0:
-                            {
-                                localPos = new Vector3(-1.164f, 0.878f ,1.8481f);
-                                collSize = new Vector2( 0.5f,4);
-                                break;
-                            }
-                            case 1:
-                            {
-                                localPos = new Vector3(-1.164f, 1.146f, 1.8481f);
-                                collSize = new Vector2(4,0.5f );
-                                break;
-                            }
-                            case 2:
-                            {
-                                localPos = new Vector3(2.0315f, 0.878f ,1.8481f);
-                                collSize = new Vector2(0.5f,4);
-                                break;
-                            }
-                            case 3:
-                            {
-                                localPos = new Vector3(-1.3315f, -1.346f ,1.8481f);
-                                collSize = new Vector2(4,0.5f);
-                                break;
-                            }
-                           
-                            default:
-                                break;
-                        }
 
-                        collider.size = collSize;
-                        colliderG.transform.parent = collidersStor.transform;
-                        colliderG.transform.localPosition = localPos;
+        public static GameObject TowerOnMap;
+        public static GameObject TowerOnGrid;
+        public const string TowerScene="TowerScene";
 
-                    }
-                    
-                    
-                    
-                    floor.AddComponent<SpriteRenderer>().sprite = TowerFloorSprite;
-                }
-            };
-        }
+    
 
         private void Awake()
         {
+            Harmony.CreateAndPatchAll(this.GetType().Assembly);
             ModInstance = this;
-            TowerSprite = TextureHelper.GetImageAsTexture("Artwork\\Tower.png").ConvertTexture();
-            TowerFloorSprite =Resources.Load<Sprite>("art\\gbc\\temples\\wizard\\wizard_temple_floor");
             Logger.LogMessage($"Mod {nameof(Act2Tower)} loaded!");
-            SceneManager.sceneLoaded+= delegate(Scene argScene, LoadSceneMode mode)
+            AssetBundleHelper.TryGet(Path.Combine(Paths.PluginPath, "towerprefabs"), "TOWER", out TowerOnMap);
+            AssetBundleHelper.TryGet(Path.Combine(Paths.PluginPath, "towerprefabs"), "TowerGrid", out TowerOnGrid);
+            AssetBundle.LoadFromFile(Path.Combine(Paths.PluginPath, "towerscene"));
+            SceneManager.sceneLoaded+= OnSceneManagerOnsceneLoaded;
+        }
+
+      
+
+        private void OnSceneManagerOnsceneLoaded(Scene arg0, LoadSceneMode mode)
+        {
+            if (arg0.name.Contains("GBC")|| arg0.name.Equals(TowerScene, StringComparison.InvariantCultureIgnoreCase))
             {
-                if (argScene.name == "GBC_WorldMap")
+                if (GBCEncounterManager.Instance == null|| GBCEncounterManager.Instance is null)
                 {
-                    OnGBCMapLoaded?.Invoke(argScene, GameObject.Find("Map").transform.GetChild(1).gameObject);
+                    Logger.LogMessage("Tried to create gbcencountermanager");
+                    SceneLoader.Load("GBC_Mycologist_Hut");
+                    SceneLoader.Load(arg0.name);
                 }
-            };
-            OnGBCMapLoaded += GBCMapLoaded_Handling;
-            
-            
+            }
+            if (arg0.name == "GBC_WorldMap")
+            {
 
+              
+                
+                Instantiate(TowerOnMap);
+                var g=Instantiate(TowerOnGrid);
+                var navGrid = NavigationGrid.instance;
+                for (int i = 0; i < navGrid.zones.GetLength(0); i++)
+                {
+                    for (int j = 0; j < navGrid.zones.GetLength(1); j++)
+                    {
+                        var zone = navGrid.zones[i, j];
+                        _Logger.LogInfo($"[{i} {j}]={zone?.gameObject?.name}");
+                    }
+                }
 
-
-
+                navGrid.zones[2, 3] = g.GetComponentInChildren<NavigationZone>();
+                ((NavigationZone2D)navGrid.zones[2, 3]).blockedDirections.Add(LookDirection.North);
+            }
         }
     }
 }
